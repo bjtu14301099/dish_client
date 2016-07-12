1d3b69ccc0d4eea520d74a8d249285af5f922088
yu.qi <yu.qi@qunar.com>
2016-07-12 21:46:24 +0800

    1

---
+++ app/src/main/java/com/example/deft/dish/activity/UploadMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.deft.dish.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.deft.dish.R;
+
+public class UploadMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uploadmenu);
+    }
+}
